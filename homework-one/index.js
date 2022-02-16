af063a21c524230f8d7d40b054ffb4f63a822b53
Olha Orach <olhaorach2002@gmail.com>
2022-02-16 22:27:11 +0200

    Added final changes.

--- homework-one/index.js
+++ homework-one/index.js
@@ -2,44 +2,63 @@ const applesPrice = 15.678;
 const meatPrice = 123.965;
 const oilPrice = 90.2345;
 
-document.writeln('Max Price:', Math.max(applesPrice, meatPrice, oilPrice) ); // max price
-document.writeln('Min Price:', Math.min(applesPrice, meatPrice, oilPrice) ); // min price
+console.log('Max Price:', Math.max(applesPrice, meatPrice, oilPrice) ); // max price
+console.log('Min Price:', Math.min(applesPrice, meatPrice, oilPrice) ); // min price
 
 function itemsSum(a, b, c) {
     return a + b + c;
 }
-
-document.writeln('Prices Sum: ', itemsSum(applesPrice, meatPrice, oilPrice) );
+console.log('Prices Sum: ', itemsSum(applesPrice, meatPrice, oilPrice) );
 
 let applesPriceInt = Math.floor(applesPrice);  //Int Price
 let meatPriceInt = Math.floor(meatPrice);     //Int Price
-let oilPriceInt = Math.floor(oilPrice);      //Int Price
+let oilPriceInt = Math.floor(oilPrice); 
+console.log('Int Apples Price: ', applesPriceInt);     //Int Price
+console.log('Int Meat Price: ', meatPriceInt);     //Int Price
+console.log('Int Oil Price: ', oilPriceInt);     //Int Price
 
-document.writeln( 'Round Sum: ', Math.round
+console.log( 'Round Sum: ', Math.round
 (itemsSum(applesPriceInt, meatPriceInt, oilPriceInt) / 100) * 100 );  // Round Price
 
 if (itemsSum(applesPriceInt, meatPriceInt, oilPriceInt) % 2 === 0) {  // Odd or Even
-    document.writeln('Is it Even Int Sum - ', true);
+    console.log('Is it Even Int Sum - ', true);
 } else {
-    document.writeln('Is it Odd Int Sum - ', false);
+    console.log('Is it Odd Int Sum - ', false);
 }
 
 let clientSum = 500;
 let clientChange = clientSum - (itemsSum(applesPrice, meatPrice, oilPrice));
-document.writeln("Client's change: ", clientChange);  // Client's Change
+console.log("Client's change: ", clientChange);  // Client's Change
 
 let priceAverage = (itemsSum(applesPrice, meatPrice, oilPrice)) / 3;
-document.writeln('Average Price: ', Math.floor(priceAverage * 100) / 100 );  //Average Prices
+console.log('Average Price: ', Math.floor(priceAverage * 100) / 100 );  //Average Prices
 
+//Last
 function getRandomDiscount(max) {
     return Math.floor(Math.random() * max);
 }
 
 let userPrice = prompt('Enter your price: ', 260.66);
 let userDiscount = Math.floor((userPrice * getRandomDiscount(100)) / 100);
-document.writeln(`Customer pays less on ${userDiscount}`);
+console.log(`Customer pays less on ${userDiscount}`);
 
 let cost = Math.floor((userPrice * 100) / 100) / 2;
 let pureProfit = cost - userDiscount;
 
-document.writeln('Pure profit: ', pureProfit);
+console.log('Pure profit: ', pureProfit);
+
+//Advanced
+document.writeln(
+    `<p>Max Price: ${Math.max(applesPrice, meatPrice, oilPrice)}</p>
+     <p>Min Price: ${Math.min(applesPrice, meatPrice, oilPrice)}</p>   
+     <p>Prices Sum: ${itemsSum(applesPrice, meatPrice, oilPrice)}</p>
+     <p>Int Apples Price: ${applesPriceInt}</p> 
+     <p>Int Meat Price: ${meatPriceInt}</p> 
+     <p>Int Oil Price: ${oilPriceInt}</p> 
+     <p>Round Sum: ${Math.round(itemsSum(applesPriceInt, meatPriceInt, oilPriceInt) / 100) * 100}</p>
+     <p>Is it Even Int Sum - ${true}</p>
+     <p>Client's change: ${clientChange}</p>
+     <p>Average Price: ${Math.floor(priceAverage * 100) / 100}</p>
+     <p>Customer pays less on ${userDiscount}</p>
+     <p>Pure profit: ${pureProfit}</p>`
+);
