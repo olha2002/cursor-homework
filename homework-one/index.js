ecc285e63f090a91937e083da530c64d80290f88
Olha Orach <olhaorach2002@gmail.com>
2022-02-16 20:11:11 +0200

    Base part done

--- homework-one/index.js
+++ homework-one/index.js
@@ -5,25 +5,41 @@ const oilPrice = 90.2345;
 document.writeln('Max Price:', Math.max(applesPrice, meatPrice, oilPrice) ); // max price
 document.writeln('Min Price:', Math.min(applesPrice, meatPrice, oilPrice) ); // min price
 
-let itemsSum = applesPrice + meatPrice + oilPrice;
-document.writeln('Sum: ', itemsSum); // Prices Sum
+function itemsSum(a, b, c) {
+    return a + b + c;
+}
+
+document.writeln('Prices Sum: ', itemsSum(applesPrice, meatPrice, oilPrice) );
 
 let applesPriceInt = Math.floor(applesPrice);  //Int Price
 let meatPriceInt = Math.floor(meatPrice);     //Int Price
 let oilPriceInt = Math.floor(oilPrice);      //Int Price
 
-let itemsSumInt = applesPriceInt + meatPriceInt + oilPriceInt;
-document.writeln( 'Round Sum: ', Math.round(itemsSumInt / 100) * 100 );  // Round Price
+document.writeln( 'Round Sum: ', Math.round
+(itemsSum(applesPriceInt, meatPriceInt, oilPriceInt) / 100) * 100 );  // Round Price
 
-if (itemsSumInt % 2 === 0) {  // Odd or Even
-    console.log(true);
+if (itemsSum(applesPriceInt, meatPriceInt, oilPriceInt) % 2 === 0) {  // Odd or Even
+    document.writeln(true);
 } else {
- console.log(false);
+    document.writeln(false);
 }
 
 let clientSum = 500;
-let clientChange = clientSum - (applesPrice + meatPrice + oilPrice);
+let clientChange = clientSum - (itemsSum(applesPrice, meatPrice, oilPrice));
 document.writeln("Client's change: ", clientChange);  // Client's Change
 
-let priceAverage = itemsSum / 3;
-console.log( Math.floor(priceAverage * 100) / 100 );  //Average Prices
+let priceAverage = (itemsSum(applesPrice, meatPrice, oilPrice)) / 3;
+document.writeln('Average Price: ', Math.floor(priceAverage * 100) / 100 );  //Average Prices
+
+function getRandomDiscount(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let userPrice = prompt('Enter your price: ', 260.66);
+let userDiscount = Math.floor((userPrice * getRandomDiscount(100)) / 100);
+console.log(`Customer pays less on ${userDiscount}`);
+
+let cost = Math.floor((userPrice * 100) / 100) / 2;
+let wholeProfit = cost - userDiscount;
+
+console.log(wholeProfit);
