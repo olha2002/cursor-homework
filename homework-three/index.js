0bff7d4b9966ca3bc9d2b1c7f6ffc5ce5417aa8f
Olha Orach <olhaorach2002@gmail.com>
2022-02-23 18:05:58 +0200

    Done task 1,2,3,5.

--- homework-three/index.js
+++ homework-three/index.js
@@ -1,4 +1,17 @@
 
+//Task 1. GetMaxDigit function
+function getMaxDigit(number) {
+    number = number.toString();
+
+    let maxNumber = number[0];
+    for (let i = 1; i < number.length; i++) {
+        if (number[i] > maxNumber) {
+            maxNumber = number[i];
+        }
+    }
+    return Number(maxNumber);
+}
+
 //Task 2. Pow function
 let calcNumberPow = function(x, n) {
     let result = x;
@@ -13,3 +26,21 @@ let calcNumberPow = function(x, n) {
     
     return result;
 };
+
+//Task 3. GetFirstLetterUpperCase
+function getFirstLetterUpperCase(name) {
+    let firstLetter = name[0].toUpperCase();
+    let otherLetters = name.slice(1, name.length);
+    name = otherLetters.toLowerCase();
+
+    let result = firstLetter + name;
+    return result;
+}
+
+//Task 5. GetRandomNumber function
+function getRandomNumber(n, m) {
+    n = Math.ceil(n);
+    m = Math.floor(m);
+ 
+    return Math.floor(Math.random() * (m - n) + n);
+ }
