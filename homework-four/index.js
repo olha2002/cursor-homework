c943c8545e2be10c822d7d7273b4cf93cd01c65d
Olha Orach <olhaorach2002@gmail.com>
2022-05-31 11:20:57 +0100

    Final changes in hw 4 i hope.

--- homework-four/index.js
+++ homework-four/index.js
@@ -2,42 +2,33 @@ const students = ['Oleksandr', 'Igor', 'Olena', 'Ira', 'Oleksii', 'Svitlana'];
 const themes = ['Differential equation', 'Theory of automata', 'Algorithms and data structures'];
 const marks = [4, 5, 5, 3, 4, 5];
 
-let studentsClone = [...students];
+const studentsClone = [...students];
 
 const boysArray = studentsClone.filter(student => !student.endsWith('a'));
 const girlsArray = studentsClone.filter(student => student.endsWith('a'));
 
 // func that combines students 
-function combineBoth(arr1, arr2) {
-  return arr1.map((e, i) => [e, arr2[i]]);
+function combineBoth(boysArray, girlsArray) {
+  return boysArray.map((boy, i) => [boy, girlsArray[i]]);
 }
 const combinedBoysGirls = combineBoth(boysArray, girlsArray);
 
  //func that get tasks to the array with pairs of students
- function getTasks(arr, arr1) {
-  const newPair = arr.map((el) => el.join(' and '));
-  return newPair.map( (arr, i) => [arr, arr1[i]] );
-  }
+ function getTasks(combinedStudents, themes) {
+  const newPair = combinedStudents.map((el) => el.join(' and '));
+  return newPair.map( (combinedStudents, i) => [combinedStudents, themes[i]] );
+}
  const pairsWithTasks = getTasks(combinedBoysGirls, themes);
 
 //func that gets student with his mark
-function studentsMarks(arr1, arr2) {
-  let marksArray = [];
-
-  for (let i = 0; i < arr1.length; i++) {
-    marksArray.push( arr1.splice(0, 1), arr1.splice(0, 1), arr1.splice(0, 1)  );
-  }
-  for (let j = 0; j < arr2.length; j++) {
-      marksArray[j].push(arr2[j])
-  }
-
-  return marksArray;
+function studentsMarks(students, marks) {
+  return students.map( (student, i)  => [student, marks[i]] );
 }
 const studentsWithMarks = studentsMarks(studentsClone, marks);
 
 //func that calcs students with their marks for the project
-function studentsEvaluatedProjects(arr1) {
-  return arr1.map((pairsAndTheirTasks) => [...pairsAndTheirTasks, Math.ceil(Math.random() * 5)]);
+function studentsEvaluatedProjects(pairsWithTasksArr) {
+  return pairsWithTasksArr.map((pairsAndTheirTasks) => [...pairsAndTheirTasks, Math.ceil(Math.random() * 5)]);
 }
 const studentsEvaluatedProjectsArr = studentsEvaluatedProjects(pairsWithTasks);
 
