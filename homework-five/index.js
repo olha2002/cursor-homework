7cdd670dc5f087db79d313e414145b9e7feef07b
Olha Orach <olhaorach2002@gmail.com>
2022-06-06 00:45:00 +0100

    Added additional things.

--- homework-five/index.js
+++ homework-five/index.js
@@ -16,18 +16,26 @@ const getRandomArray = (length, min, max) => {
     return randomArr;
 }
 const randomArray = getRandomArray(8, 55, 100);
-console.log('My Random Array: ', randomArray );
+console.log( `My Random Array: ${randomArray}` );
 
 // Function 2.
 /*const getModa = (...numbers) => {
     const modaArr = Array.from(numbers);
-    const filteredModaArray = modaArr.filter(value => typeof value === 'number' && parseInt(value));
+    const filteredModaArray = modaArr.filter(value => Number(value) && parseInt(value) === value);
     console.log(filteredModaArray);
 
+    const occurences = {};
+    filteredModaArray.forEach(element => {
+        occurences[element] = occurences[element] + 1 || 1;
+    });
 
+    const elementOccurance = Object.values(occurences);
+    const max = Math.max(...elementOccurance);
+
+    return max;
 }
 
-console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log(getModa(6, 2, 6, 6, 8, 9, 9, 9, 0));
 */
 // Function 3. Gets the average value
 function getAverage(...numbers) {
@@ -40,7 +48,7 @@ function getAverage(...numbers) {
    return Number(sumOfArrayElements / averageArr.length);
 } 
 const averageArray = getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2);
-console.log('My getAverage num: ', averageArray);
+console.log( `My getAverage num: ${averageArray}` );
 
 // Function 4. Gets numbers median
 function getMedian(...numbers) {
@@ -61,28 +69,28 @@ function getMedian(...numbers) {
 }
 
 const median = getMedian(1, 2, 3, 4, 5, 6, 8, 9);
-console.log('My Median: ', median );
+console.log( `My Median: ${median}` );
 // Function 5. Filters even numbers
 const filterEvenNumbers = (...numbers) => {
-    return Array.from(numbers).filter(value => value % 2 !== 0);
+    return Array.from(numbers).filter(value => Number(value) && value % 2 !== 0);
 }
-const filterEvenNumbersArray = filterEvenNumbers(1, 2, 3, 4, 5, 6, 8, 8, 9);
-console.log('Filtered with EvenNumbers Array', filterEvenNumbersArray);
+const filterEvenNumbersArray = filterEvenNumbers(1, 2, 3, 'g', 4, 5, 6, 8, 8, 9);
+console.log( `Filtered with EvenNumbers Array: ${filterEvenNumbersArray}` );
 
 // Function 6. Counts positive numbers
 const countPositiveNumbers = (...numbers) => {
-   const positiveNumbersArray = Array.from(numbers).filter(value => value > 0);
+   const positiveNumbersArray = Array.from(numbers).filter(value => Number(value) && value > 0);
    return positiveNumbersArray.length;
 }
 const countPositiveNumbersArr = countPositiveNumbers(1, -2,  3, -4, -5,  6, -1);
-console.log('Positives numbers: ', countPositiveNumbersArr);
+console.log( `Positives numbers: ${countPositiveNumbersArr}` );
 
 // Function 7. Gets divided by 5 numbers
 const getDividedByFive = (...numbers) => {
-    return Array.from(numbers).filter(value => value % 5 === 0);
+    return Array.from(numbers).filter(value => Number(value) && value % 5 === 0);
 }
 const dividedByFiveArray = getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2);
-console.log('Divided by 5 numbers: ', dividedByFiveArray );
+console.log( `Divided by 5 numbers: ${dividedByFiveArray}` );
 
 // Function 8. Replaces bad word by ****
 function replaceBadWords(string, addYoursBadWord) {
@@ -109,19 +117,22 @@ function replaceBadWords(string, addYoursBadWord) {
 }
 
 const replacedBadWords = replaceBadWords("Are you fucking kidding?");
-console.log('Replaced Bad words: ', replacedBadWords);
+console.log( `Replaced Bad words: ${replacedBadWords}` );
 
 // Function 9. Divides word by three parts
 function divideByThree(word) {
-    return word.toLowerCase().replaceAll(' ', '').match(/.{1,3}/g);
+    if (word !== '') {
+        return String(word).toLowerCase().replaceAll(' ', '').match(/.{1,3}/g);
+    } else {
+        return 'Enter your word!';
+    }
 }
-const dividedByThreeArray = divideByThree('Commander');
-console.log('Divided by 3 Array: ', dividedByThreeArray);
+const dividedByThreeArray = divideByThree('live');
+console.log( `Divided by 3 Array: ${dividedByThreeArray}` );
 
 // Function 10. 
 /*function generateCombinations(word) {
+    
+}
 
-
-    const result = words.filter(word => word.length > 10);
-    return result;
-}*/
+console.log( generateCombinations("man") ); */
