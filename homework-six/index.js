f9c374bbe26b89057ca1118a49b1b3b4e8be17e8
Olha Orach <olhaorach2002@gmail.com>
2022-06-10 18:26:12 +0100

    Done tasks 3-5.

--- homework-six/index.js
+++ homework-six/index.js
@@ -36,6 +36,7 @@ console.log('Subjects of student Tanya:', getSubjects(students[0]));
 console.log('Subjects of student Victor:', getSubjects(students[1]));
 console.log('Subjects of student Anton:', getSubjects(students[2]));
 
+// Function #2 that gets student's average mark
 function getAverageMark(studentsArray) {
     const splitedMarks = Object.values(studentsArray.subjects);
     const concatedMarks = splitedMarks.flat();
@@ -51,3 +52,39 @@ function getAverageMark(studentsArray) {
 console.log('Average mark of student Tanya: ', getAverageMark(students[0]));
 console.log('Average mark of student Victor: ', getAverageMark(students[1]));
 console.log('Average mark of student Anton: ', getAverageMark(students[2]));
+
+// Function #3 that gets information about specific student
+const getStudentInfo = (studentsArray) => {
+   return {
+    name: studentsArray.name,
+    course: studentsArray.course,
+    averageMark: getAverageMark(studentsArray)
+   }
+};
+
+console.log('Info about Tanya: ', getStudentInfo(students[0]));
+console.log('Info about Victor: ',getStudentInfo(students[1]));
+console.log('Info about Anton: ', getStudentInfo(students[2]));
+
+// Function #4 that gets Student Names
+const getStudentsNames = (students) => {
+    return students.map(student => (student['name'])).sort();
+};
+
+console.log('Names of students in alphabetical order: ', getStudentsNames(students) );
+
+// Function #5 that gets best student
+const getBestStudent = (student) => {
+    const maxMark = Math.max(getAverageMark(student[0]), 
+                    getAverageMark(student[1]),
+                    getAverageMark(student[2]));
+
+    for (let index = 0; index < student.length; index++) {
+        if (getStudentInfo(student[index])['averageMark'] === maxMark ) {
+            return student[index]['name'];
+        }
+    }
+};
+
+console.log('The best student is: ', getBestStudent(students));
+
