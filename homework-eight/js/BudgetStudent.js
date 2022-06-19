7381196ddda2f83823e45546be3f0a26f76161b1
Olha Orach <olhaorach2002@gmail.com>
2022-06-19 18:55:08 +0100

    Done advanced tasks 1-3

--- homework-eight/js/BudgetStudent.js
+++ homework-eight/js/BudgetStudent.js
@@ -1,3 +1,14 @@
 class BudgetStudent extends Student {
-    
+
+    constructor(university, course, fullName, scholarship) {
+        super(university, course, fullName);
+        this._scholarship = scholarship;
+        this.getScholarship();
+    }
+
+    getScholarship() {
+        setInterval(() => {
+            console.log( `You've got ${this._scholarship} of your scholarsip!` );
+        }, '30000');
+    } 
 }
--- homework-eight/js/index.js
+++ homework-eight/js/index.js
@@ -3,17 +3,20 @@ const student = new Student('Lviv Polytechnic', 'third', 'Olha Orach');
 student.marks = 5;
 console.log( 'Student info: ', student.getInfo() );
 console.log( 'Student Marks: ', student.marks );
-console.log('Average mark: ', student.getAverageMark());
+console.log('Average mark: ', student.getAverageMark() );
 
 student.dismiss();
 
 student.marks = 2;
 console.log( 'Student Marks: ', student.marks );
 console.log( 'Student info: ', student.getInfo() );
-console.log('Average mark: ', student.getAverageMark());
+console.log('Average mark: ', student.getAverageMark() );
 
 student.recover();
 
 console.log( 'Student Marks: ', student.marks );
 console.log( 'Student info: ', student.getInfo() );
-console.log('Average mark: ', student.getAverageMark());
+console.log('Average mark: ', student.getAverageMark() );
+
+const studentTwo = new BudgetStudent('Lviv Polytechnic', 'third', 'Olha Orach', 1400);
+
