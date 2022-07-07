edcda4d688fefdc5653baa48accb4deff88f31de
Olha Orach <olhaorach2002@gmail.com>
2022-07-07 01:40:32 +0100

    Done hw 12

--- homework-twelve/js/index.js
+++ homework-twelve/js/index.js
@@ -6,23 +6,23 @@ const nextButton = document.querySelector(".btn-next");
 
 //variables
 const BASE_URL = "https://swapi.dev/api/";
-const elementIndex = 0;
+let elementIndex = 0;
 
 const charactersContainer = document.createElement("div");
-charactersContainer.className = "container";
+charactersContainer.className = "container container-one";
 infoButton.after(charactersContainer);
 
 const planetsContainer = document.createElement("div");
 planetsContainer.className = "container planets-container";
 planetsButton.after(planetsContainer);
 
-
 // functions
 async function getInfo() {
-  const episodeResponse = await fetch(`${BASE_URL}/films/2`);
-  const episode = await episodeResponse.json();
+  const episodeResponse = await fetch(`${BASE_URL}/films/2`).then((response) =>
+    response.json()
+  );
 
-  for (let promise of episode.characters) {
+  for (promise of episodeResponse.characters) {
     const hero = await fetch(promise).then((res) => res.json());
     charactersContainer.insertAdjacentHTML(
       "beforeend",
@@ -44,23 +44,25 @@ async function getInfo() {
   }
 }
 
-async function getPlanets(index) {
-  const planetsResponse = await fetch(`${BASE_URL}/planets/5`);
-  const planets = await planetsResponse.json();
+async function getPlanets(elementIndex) {
+  planetsContainer.innerHTML = "";
+  const { results: planetsResponse } = await fetch(`${BASE_URL}/planets/`).then(
+    (response) => response.json()
+  );
 
   planetsContainer.insertAdjacentHTML(
     "beforeend",
     `<div class="character flip-card planets-container">
         <div class="flip-card-inner">
         <div class="flip-card-front">
-        <img src="./img/planets/${planets.name}.jpg" class="planets-img" alt="Picture no found!">
-            <span id="planet-name">${planets.name}</span>
+        <img src="./img/planets/${planetsResponse[elementIndex].name}.jpg" class="planets-img" alt="Picture no found!">
+            <span id="planet-name">${planetsResponse[elementIndex].name}</span>
       </div>
         <div class="flip-card-back">
-          <span id="planet-diameter">Diameter: ${planets.diameter}</span>
-          <span id="planet-films">Population: ${planets.population}</span>
-          <span id="planet-climate">Climate: ${planets.climate}</span>
-          <span id="planet-terrain">Terrain: ${planets.terrain}</span>
+          <span id="planet-diameter">Diameter: ${planetsResponse[elementIndex].diameter}</span>
+          <span id="planet-films">Population: ${planetsResponse[elementIndex].population}</span>
+          <span id="planet-climate">Climate: ${planetsResponse[elementIndex].climate}</span>
+          <span id="planet-terrain">Terrain: ${planetsResponse[elementIndex].terrain}</span>
       </div>
       </div>
       </div>
@@ -70,4 +72,14 @@ async function getPlanets(index) {
 
 // logic
 infoButton.addEventListener("click", getInfo);
-planetsButton.addEventListener("click", getPlanets);
+planetsButton.addEventListener("click", () => {
+  getPlanets(elementIndex);
+});
+previoutButton.addEventListener("click", () => {
+  elementIndex > 0 ? elementIndex-- : (elementIndex = 9);
+  getPlanets(elementIndex);
+});
+nextButton.addEventListener("click", () => {
+  elementIndex < 9 ? elementIndex++ : (elementIndex = 0);
+  getPlanets(elementIndex);
+});
