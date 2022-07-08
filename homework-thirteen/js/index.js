4d6c3918c0f7e40a791fddfa1b6f841cea3078b6
Olha Orach <olhaorach2002@gmail.com>
2022-07-08 16:10:36 +0100

    Did tasks 1 and 2

--- homework-thirteen/js/index.js
+++ homework-thirteen/js/index.js
@@ -1,10 +1,12 @@
 // selectors
 const FONT_SIZE = 14;
 const task1 = document.querySelector(".task-1");
+const task2 = document.querySelector(".task-2");
+const documentBody = document.querySelector("body");
 
 //variables
 const idButton = document.createElement("button");
-idButton.className = "id";
+idButton.className = "id id-btn";
 idButton.innerHTML = "Generate Me";
 
 task1.after(idButton);
@@ -13,6 +15,23 @@ const idResult = document.createElement("span");
 idResult.id = "result";
 
 idButton.after(idResult);
+
+const makeLessButton = document.createElement("button");
+makeLessButton.className = "id less-btn";
+makeLessButton.innerHTML = "Make less";
+
+task2.after(makeLessButton);
+
+const makeBiggerButton = document.createElement("button");
+makeBiggerButton.className = "id big-btn";
+makeBiggerButton.innerHTML = "Make Bigger";
+
+makeLessButton.after(makeBiggerButton);
+
+const fontResult = document.createElement("span");
+fontResult.id = "span-result";
+
+makeBiggerButton.after(fontResult);
 
 //functions
 function* createIdGenerator() {
@@ -24,10 +43,25 @@ function* createIdGenerator() {
   }
 }
 
-function* newFontGenerator(FONT_SIZE) {}
+function* newFontGenerator(fontSize) {
+  let counter;
+
+  while (true) {
+    counter = yield fontSize;
+
+    if (counter === "up") {
+      fontSize += 2;
+    }
+
+    if (counter === "down") {
+      fontSize -= 2;
+    }
+  }
+}
 
 //logic
 const idGenerator = createIdGenerator();
+
 idButton.addEventListener("click", () => {
   idResult.style.cssText = `
     margin-left: 2rem;
@@ -35,4 +69,28 @@ idButton.addEventListener("click", () => {
     padding: 0.8rem;
     `;
   idResult.textContent = idGenerator.next().value;
+});
+
+const fontGenerator = newFontGenerator(FONT_SIZE);
+
+makeLessButton.addEventListener("click", () => {
+  fontResult.style.cssText = `
+    border: 2px solid crimson;
+    padding: 0.8rem;
+    `;
+
+  const fontSize = fontGenerator.next("down").value;
+  documentBody.style.fontSize = `${fontSize}px`;
+  fontResult.textContent = `${fontSize}px`;
+});
+
+makeBiggerButton.addEventListener("click", () => {
+  fontResult.style.cssText = `
+    border: 2px solid crimson;
+    padding: 0.8rem;
+    `;
+
+  const fontSize = fontGenerator.next("up").value;
+  documentBody.style.fontSize = `${fontSize}px`;
+  fontResult.textContent = `${fontSize}px`;
 });
